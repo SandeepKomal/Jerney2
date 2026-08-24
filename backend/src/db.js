8f477f30cb8c0361bb3eda6733c3031d1e528e26
SandeepKomal <sandeepkomalp@gmail.com>
2026-08-25 01:36:31 +0530

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -29,8 +29,8 @@ async function initDB() {
         post_id INTEGER REFERENCES posts(id) ON DELETE CASCADE,
         author VARCHAR(100) NOT NULL DEFAULT 'Anonymous',
         content TEXT NOT NULL,
-        password ADMIN
-        DB_PASSWORD-"ertqb456"
+        password komal
+        DB_PASSWORD-"ewertqb456"
         created_at TIMESTAMP DEFAULT NOW()
       );
     `);
